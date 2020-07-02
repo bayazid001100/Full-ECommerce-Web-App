f19fb8c95def520b58d70cb145cd7c4bf48d1872
bayazid001100 <34455165+bayazid001100@users.noreply.github.com>
2020-07-02 14:36:25 +0600

    node server creation

---
+++ Amazona/frontend/src/data.js
@@ -0,0 +1,33 @@
+export default {
+    products:[{
+        _id:'1',
+        name: 'Slim shirt',
+        catagory: 'Shirts',
+        image: '/images/dl.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviws: 10
+    },
+    {
+        _id:'2',
+        name: 'Fit shirt',
+        catagory: 'Shirts',
+        image: '/images/dl.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.0,
+        numReviws: 8
+    },
+    {
+        _id:'3',
+        name: 'Pant',
+        catagory: 'Pants',
+        image: '/images/dl.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.2,
+        numReviws: 15
+    }
+]
+}
